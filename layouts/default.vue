a497fc02bc1cc74d1c2bab67a825332fa32af5af
thomas <tp456@hotmail.fr>
2019-06-07 22:45:19 +0200

    added navbar and better css

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,16 @@
 <template>
-    <nuxt />
+    <div>
+        <the-navbar></the-navbar>
+        <nuxt />
+    </div>
 </template>
 
 <style>
 
 </style>
+<script>
+    import TheNavbar from "../components/the-navbar";
+    export default {
+        components: {TheNavbar}
+    }
+</script>
