6280278e4d60335f6711aaeace43e7c9c13bc293
Thomas <thomas.philippot@labo-mylab.fr>
2019-06-11 23:02:24 +0200

    project added

--- pages/index.vue
+++ pages/index.vue
@@ -4,10 +4,10 @@
       <div class="hero-body">
         <div class="container is-jumbotron">
           <h1 class="title is-spaced is-size-1-desktop is-size-2-tablet is-size-3-mobile">
-            Studdent Developper
+            Fullstack studdent Developper
           </h1>
           <h2 class="subtitle is-size-4-desktop">
-            Hello my name is Thomas.
+            I am a french developper, learning in progress.
           </h2>
           <img class="avatar" src="/img/myAvatar.svg" alt="thomas philippot avatar" width="250rem">
         </div>
@@ -17,10 +17,11 @@
       <div class="hero-body">
         <div class="container">
           <h1 class="title">
-            Primary title
+            Hi my name is Thomas.
           </h1>
-          <h2 class="subtitle">
-            Primary subtitle
+          <h2 class="subtitle has-text-white">
+            I'm learning various programming laguages since 3 years. I'm now a developper for a french company since
+            1 year.
           </h2>
         </div>
       </div>
@@ -30,12 +31,9 @@
         <div class="box">
           <div class="content">
             <div class="columns">
-              <div class="column">
+              <div class="column has-border-right">
                 <h1 class="title">Front End</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae ante ex. Vestibulum posuere dolor et massa convallis maximus. Donec vitae malesuada mi. Donec in odio augue. Vivamus mauris lectus, dapibus ornare ipsum nec, suscipit fermentum ex. Aenean id bibendum arcu. Nulla porta non libero sed congue. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam in enim at felis tincidunt mollis sed in sem. Vestibulum gravida, elit id finibus sodales, metus enim dapibus magna, sit amet cursus felis dolor sed nulla. Integer nec erat turpis. Phasellus quis mattis turpis. Sed mauris dolor, iaculis id posuere ut, hendrerit sit amet lacus. Phasellus augue odio, malesuada eget ligula vel, aliquet interdum magna. Maecenas volutpat tempor lorem sit amet pretium. Phasellus id suscipit felis.</p>
-              </div>
-              <div class="column">
-                <h1 class="title">Front 2</h1>
               </div>
               <div class="column">
                 <h1 class="title">Front 2</h1>
@@ -48,6 +46,27 @@
     <section class="section">
       <div class="container has-text-centered">
         <img src="/img/web_dev.svg" alt="web developper" width="450rem">
+      </div>
+    </section>
+    <section class="is-medium">
+      <div class="container">
+        <h1 class="title has-text-centered">The projects I made</h1>
+        <div class="columns has-text-centered">
+          <div class="column">
+            <div class="box is-hover">
+              <a href="https://esr-handball.fr" target="_blank">
+                <img src="/img/esr-hand.png" alt="my sowcase app">
+              </a>
+            </div>
+          </div>
+          <div class="column">
+            <div class="box is-hover">
+              <a href="https://esr-handball.fr" target="_blank">
+                <img src="/img/showcase.png" alt="my sowcase app">
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   </div>
