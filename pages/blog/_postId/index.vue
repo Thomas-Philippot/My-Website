a88dccc9a326f4c3d893207a9c74d0f9303e19a8
Thomas <thomas.philippot@labo-mylab.fr>
2019-11-25 12:30:52 +0100

    tags and comment/like count on blog page

--- pages/blog/_postId/index.vue
+++ pages/blog/_postId/index.vue
@@ -1,7 +1,37 @@
 <template>
     <div class="container">
         <the-jumbotron :list="jumbotron"></the-jumbotron>
-        <h1 class="title">{{ article.title }}</h1>
+        <div class="columns is-centered">
+          <div class="column is-half">
+            <img :src="article.cover_image" :alt="article.title">
+          </div>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <h1 class="is-size-1 title">{{ article.title }}</h1>
+            <div class="tags">
+              <span
+                v-for="tag in article.tags"
+                :key="tag"
+                class="tag is-link"
+              >#{{ tag }}</span>
+            </div>
+          </div>
+          <div class="column is-one-fifth">
+            <div class="button is-primary is-outlined is-rounded">
+              <span class="icon is-small">
+                <i class="fas fa-heart"></i>
+              </span>
+              <span class="">{{ article.positive_reactions_count }}</span>
+            </div>
+            <div class="button is-primary is-outlined is-rounded">
+              <span class="icon is-small">
+                <i class="fas fa-comment"></i>
+              </span>
+              <span class="">{{ article.comments_count }}</span>
+            </div>
+          </div>
+        </div>
         <div class="blog-content" v-html="article.body_html"></div>
         <nuxt-link to="/blog" class="button is-primary is-outlined">Back</nuxt-link>
     </div>
@@ -49,5 +79,12 @@
 </script>
 
 <style scoped>
+
+  div.social {
+    color: black;
+    border: 2px solid black;
+    border-radius: 100%;
+    margin: 0 7px;
+  }
 
 </style>
